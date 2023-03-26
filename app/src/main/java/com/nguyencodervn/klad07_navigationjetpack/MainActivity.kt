81a3233fee101dcfad797a84de523378d8a1b47e
NguyenCoderVN <41292507+NguyenCoderVN@users.noreply.github.com>
2023-03-26 07:08:11 +0700

    Commit

--- app/src/main/java/com/nguyencodervn/klad07_navigationjetpack/MainActivity.kt
+++ app/src/main/java/com/nguyencodervn/klad07_navigationjetpack/MainActivity.kt
@@ -1,36 +1,52 @@
 package com.nguyencodervn.klad07_navigationjetpack
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.Toolbar
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.onNavDestinationSelected
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var toolbar: MaterialToolbar
     private lateinit var bottomNavView: BottomNavigationView
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navView: NavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         toolbar = findViewById(R.id.toolbar)
+        drawerLayout = findViewById(R.id.drawer_layout)
         bottomNavView = findViewById(R.id.bottom_nav)
 
         setSupportActionBar(toolbar)
+
+        val actionBar: ActionBar? = supportActionBar
+        actionBar?.setDisplayHomeAsUpEnabled(false)
+
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         val navController = navHostFragment.navController
+
         val builder = AppBarConfiguration.Builder(navController.graph)
+        builder.setOpenableLayout(drawerLayout)
+
         val appBarConfiguration = builder.build()
         toolbar.setupWithNavController(navController,appBarConfiguration)
 
+
+        navView = findViewById(R.id.nav_view)
+        NavigationUI.setupWithNavController(navView, navController)
         bottomNavView.setupWithNavController(navController)
     }
 
