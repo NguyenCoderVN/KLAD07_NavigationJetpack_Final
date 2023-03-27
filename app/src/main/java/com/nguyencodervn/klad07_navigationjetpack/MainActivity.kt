24514cd5b0c7a44a85275292d137768c022cd062
NguyenCoderVN <41292507+NguyenCoderVN@users.noreply.github.com>
2023-03-27 13:47:17 +0700

    Commit

--- app/src/main/java/com/nguyencodervn/klad07_navigationjetpack/MainActivity.kt
+++ app/src/main/java/com/nguyencodervn/klad07_navigationjetpack/MainActivity.kt
@@ -1,17 +1,18 @@
 package com.nguyencodervn.klad07_navigationjetpack
 
+import android.os.Build
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import androidx.appcompat.app.ActionBar
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.onNavDestinationSelected
-import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
@@ -21,33 +22,32 @@ class MainActivity : AppCompatActivity() {
     private lateinit var bottomNavView: BottomNavigationView
     private lateinit var drawerLayout: DrawerLayout
     private lateinit var navView: NavigationView
+    private lateinit var navHostFragment: NavHostFragment
+    private lateinit var navController: NavController
 
+    @RequiresApi(Build.VERSION_CODES.R)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        window.decorView.windowInsetsController!!.hide(
+//            android.view.WindowInsets.Type.statusBars()
+//        )
+
         toolbar = findViewById(R.id.toolbar)
-        drawerLayout = findViewById(R.id.drawer_layout)
         bottomNavView = findViewById(R.id.bottom_nav)
+        navView = findViewById(R.id.nav_view)
+        drawerLayout = findViewById(R.id.drawer_layout)
+
+        navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
 
         setSupportActionBar(toolbar)
-
-        val actionBar: ActionBar? = supportActionBar
-        actionBar?.setDisplayHomeAsUpEnabled(false)
-
-        val navHostFragment = supportFragmentManager
-            .findFragmentById(R.id.fragmentContainerView) as NavHostFragment
-        val navController = navHostFragment.navController
-
-        val builder = AppBarConfiguration.Builder(navController.graph)
-        builder.setOpenableLayout(drawerLayout)
-
-        val appBarConfiguration = builder.build()
-        toolbar.setupWithNavController(navController,appBarConfiguration)
-
-
-        navView = findViewById(R.id.nav_view)
+        NavigationUI.setupWithNavController(toolbar, navController, drawerLayout)
         NavigationUI.setupWithNavController(navView, navController)
-        bottomNavView.setupWithNavController(navController)
+        NavigationUI.setupWithNavController(bottomNavView, navController)
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -58,6 +58,15 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         val navController = findNavController(R.id.fragmentContainerView)
         return item.onNavDestinationSelected(navController)
-                ||  super.onOptionsItemSelected(item)
+                || super.onOptionsItemSelected(item)
+    }
+    @Deprecated("Deprecated in Java")
+    override fun onBackPressed() {
+
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
     }
 }
