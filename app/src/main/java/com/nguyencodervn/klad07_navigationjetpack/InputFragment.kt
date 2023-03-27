0d82117dc4d883b6404ce598d4a03822d55f7538
NguyenCoderVN <41292507+NguyenCoderVN@users.noreply.github.com>
2023-03-27 21:46:31 +0700

    Commit

--- app/src/main/java/com/nguyencodervn/klad07_navigationjetpack/InputFragment.kt
+++ app/src/main/java/com/nguyencodervn/klad07_navigationjetpack/InputFragment.kt
@@ -1,11 +1,5 @@
 package com.nguyencodervn.klad07_navigationjetpack
 
-import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 
 class InputFragment : Fragment(R.layout.fragment_input)
--- app/src/main/java/com/nguyencodervn/klad07_navigationjetpack/MainActivity.kt
+++ app/src/main/java/com/nguyencodervn/klad07_navigationjetpack/MainActivity.kt
@@ -1,23 +1,7 @@
 package com.nguyencodervn.klad07_navigationjetpack
 
-import android.os.Build
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
-import android.widget.Toast
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.GravityCompat
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.navigation.NavController
-import androidx.navigation.findNavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.NavigationUI
-import androidx.navigation.ui.onNavDestinationSelected
-import com.google.android.material.appbar.MaterialToolbar
-import com.google.android.material.badge.BadgeDrawable
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
 
